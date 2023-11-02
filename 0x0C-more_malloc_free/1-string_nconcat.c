f571bfcee18dd858e7e26952070b45cb569c5cb4
RalphOkorie <okorieralphc@gmail.com>
2023-11-02 08:50:21 -0700

    updated

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,8 +1,8 @@
-i#include "main.h"
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * string_nconcat - this concatenates two strings.
+ * string_nconcat - concatenates two strings.
  * @s1: first string.
  * @s2: second string.
  * @n: amount of bytes.
