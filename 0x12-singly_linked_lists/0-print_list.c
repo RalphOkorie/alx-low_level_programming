ab73595165393ba495939cd97eb9da8af73b94f9
RalphOkorie <okorieralphc@gmail.com>
2023-11-16 19:07:18 -0800

    fixed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,27 +3,27 @@
 #include <stdlib.h>
 
 /**
- * print_list - prints all the elements of a list_t list.
+ * print_list - Prints all the elements of a list_t list.
  * @h: The list_t list.
  *
- * Return: The number of inodes in h.
+ * Return: The number of nodes in h.
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t inodes = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", 0, "(nil)");
 
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
-		inodes++;
+		nodes++;
 		h = h->next;
 	}
 
-	return (inodes);
+	return (nodes);
 }
